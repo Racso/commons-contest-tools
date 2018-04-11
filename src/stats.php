c02203c8b5e5e394380374795c21aea1d840d7e8
Racso <correo@oscargomez.net>
2018-04-10 19:49:04 -0500

    Extracting more methods.

--- src/stats.php
+++ src/stats.php
@@ -14,29 +14,37 @@ if (!$db)
 
 include_once("updateCache.php");
 
-//== Cargar datos de los municipios colombianos ==
-$monuments = array();
-$monuments["?"] = array('id'=>"?", 'departamento'=>'?', 'municipio' => '?');
-
-$res = $db->query("SELECT id, topName, name FROM places");
-while ($r = $res->fetchArray(SQLITE3_ASSOC))
-{
-	$monuments[$r['id']] = array('id'=>$r['id'], 'departamento'=>$r['topName'], 'municipio'=>$r['name']);
-}
+$monuments = loadMonumentsData();
 
 showHeader();
 
 if (isset($_GET['gallery']))
 {
-    exec_gallery();
+    showGallery();
 }
 else
 {
-    exec_statistics();
+    showStatistics();
 }
 
 showFooter();
 exit();
+
+function loadMonumentsData()
+{
+	global $db;
+
+	$monuments = array();
+	$monuments["?"] = array('id'=>"?", 'departamento'=>'?', 'municipio' => '?');
+
+	$queryResult = $db->query("SELECT id, topName, name FROM places");
+	while ($row = $queryResult->fetchArray(SQLITE3_ASSOC))
+	{
+		$monuments[$row['id']] = array('id'=>$row['id'], 'departamento'=>$row['topName'], 'municipio'=>$row['name']);
+	}
+
+	return $monuments;
+}
 
 function showHeader()
 {
@@ -59,95 +67,10 @@ function showHeader()
         </p>
         <hr />
 	</center>
-    <?php
-    }
-
-    function exec_statistics()
-    {
-    global $db, $monuments, $projectName, $projectLogo, $projectURL;
-    //== Calcular datos ==
-    //=== Estadísticas básicas ===
-    //Total de imágenes subidas
-    $out_total_subidas = $db->querySingle("SELECT COUNT(*) FROM photos;");
-    //=== Estadísticas por persona ===
-    $out_datos_personas = array();
-    $res = $db->query("SELECT author,COUNT(*) total,COUNT(DISTINCT place) places FROM photos GROUP BY author ORDER BY total DESC,places DESC");
-    while ($r = $res->fetchArray(SQLITE3_ASSOC)) {
-    $out_datos_personas[$r['author']] = array('total'=>$r['total'], 'places'=>$r['places']);
-    }
-    //=== Estadísticas por departamento ===
-    $out_datos_departamentos = array();
-    $res = $db->query("select ifnull(topname,'?') topName,count(*) total,count(distinct name) places from photos left outer join places on photos.place=places.id group by topname order by total desc,places desc");
-    while ($r = $res->fetchArray(SQLITE3_ASSOC)) {
-    $out_datos_departamentos[$r['topName']] = array('total'=>$r['total'], 'places'=>$r['places']);
-    }
-    //=== Estadísticas por municipio ===
-    $out_datos_municipios = array();
-    $res = $db->query("SELECT ifnull(id,'?') id,ifnull(name,'?') name,COUNT(*) total, COUNT(DISTINCT author) people
-    FROM photos LEFT OUTER JOIN places ON photos.place=places.id GROUP BY id,name ORDER BY total DESC,people DESC");
-    while ($r = $res->fetchArray(SQLITE3_ASSOC)) {
-    $out_datos_municipios[$r['id']] = array('name'=>$r['name'], 'total'=>$r['total'], 'people'=>$r['people']);
-    }
-
-    ?>
-	<center>
-        <table style="text-align:center; border:none;">
-            <tr>
-                <td><img src="<?=$projectLogo?>" /></td>
-                <td>
-                    <span style="font-size:xx-large"><a href="<?=$projectURL?>"><?=$projectName?></a></span><br />
-                    <span style="font-size:large">Estadísticas y datos de interés</span>
-                </td>
-            </tr>
-        </table>
-        <div style="border:1px dotted; display: inline-block; padding: 15px;">
-            <p style="font-size:small;">Hasta el momento,</p>
-            <p style="font-size:large;font-weight:bold;"><?=sizeof($out_datos_personas)?> participantes</p>
-            <p style="font-size:small;">han subido</p>
-            <p style="font-size:x-large;font-weight:bold;"><?=$out_total_subidas?> fotografías</p>
-            <p style="font-size:small;">con las cuales han captado</p>
-            <p style="font-size:large;font-weight:bold;"><?=sizeof($out_datos_municipios)?> municipios</p>
-        </div>
-        <h1>Los departamentos</h1>
-
-		<table class="wikitable" style="text-align: center;">
-            <tr><th>Departamento</th><th>Fotos totales</th><th>Municipios<br />fotografiados</th></tr>
-            <?php foreach ($out_datos_departamentos as $nombre=>$departamento): ?>
-            <tr><td><a href="?gallery&departamento=<?=$nombre?>"><?=$nombre?></a></td><td><?=$departamento['total']?></td><td><?=$departamento['places']?></td></tr>
-			<?php endforeach; ?>
-        </table>
-        <table style="border:none;" width="90%">
-            <tr>
-                <td valign="top">
-                    <center>
-                        <h1>Los participantes</h1>
-                        <table class="wikitable" style="text-align: center;">
-                            <tr><th>Participante</th><th>Fotos totales</th><th>Municipios<br />fotografiados</th></tr>
-                            <?php foreach ($out_datos_personas as $nombre=>$persona): ?>
-                            <tr><td><a href="?gallery&participante=<?=rawurlencode ($nombre)?>"><?=$nombre?></a></td><td><?=$persona['total']?></td><td><?=$persona['places']?></td></tr>
-							<?php endforeach; ?>
-                        </table>
-                    </center>
-                </td>
-                <td valign="top">
-                    <center>
-                        <h1>Los municipios</h1>
-                        <table class="wikitable" style="text-align: center;">
-                            <tr><th>Código</th><th>Municipio</th><th>Fotos totales</th><th>Participantes</th></tr>
-							<?php foreach ($out_datos_municipios as $id => $monumento): ?>
-                            <tr><td><a href="?gallery&municipio=<?=$id?>"><?=$id?></a></td><td><?=$monumento['name']?></td><td><?=$monumento['total']?></td><td><?=$monumento['people']?></td></tr>
-							<?php endforeach; ?>
-                        </table>
-                    </center>
-                </td>
-            </tr>
-        </table>
-    </center>
-<?php
-
+	<?php
 }
 
-function exec_gallery()
+function showGallery()
 {
     global $db, $projectName, $projectLogo, $projectURL;
 
@@ -201,17 +124,26 @@ function exec_gallery()
     $cols = 5;
     $curImg = 0;
 
-?>
+    ?>
 	<center>
-		<h1><?=$nombreGaleria?><span style="font-size:60%;"><br/>Galería del <?=$filter_type?></span></h1>
-		<p><a href="?">[ Volver a las estadísticas ]</a></p>
+		<h1>
+			<?=$nombreGaleria?>
+			<span style="font-size:60%;">
+				<br />Galería del <?=$filter_type?>
+			</span>
+		</h1>
+		<p>
+			<a href="?">[ Volver a las estadísticas ]</a>
+		</p>
 		<table style="text-align: center; border: none;">
 			<?php while ($curImg<sizeof($gallery)): ?>
 			<tr>
 				<?php for ($i=0 ; $i<$cols; $i++): ?>
 				<td>
 					<?php if ($curImg<sizeof($gallery)): ?>
-					<a href="http://commons.wikimedia.org/wiki/<?=rawurlencode($gallery[$curImg]['title'])?>" target="_blank"><img src="<?=$gallery[$curImg]['url']?>" /></a>
+					<a href="http://commons.wikimedia.org/wiki/<?=rawurlencode($gallery[$curImg]['title'])?>" target="_blank">
+						<img src="<?=$gallery[$curImg]['url']?>" />
+					</a>
 					<?php endif; ?>
 				</td>
 				<?php $curImg+=1;
@@ -219,13 +151,118 @@ function exec_gallery()
 			</tr>
 			<?php endwhile; ?>
 		</table>
-    </center>';
-<?php
+	</center>';
+	<?php
+}
+
+function showStatistics()
+{
+    global $db, $monuments, $projectName, $projectLogo, $projectURL;
+
+    $totalNumberOfImages = $db->querySingle("SELECT COUNT(*) FROM photos;");
+
+    $statsByParticipant = getStatsByParticipant();
+	$statsByDepartment = getStatsByZone();
+	$statsByLocation = getStatsByLocation();
+
+	?>
+	<center>
+        <table style="text-align:center; border:none;">
+            <tr>
+                <td><img src="<?=$projectLogo?>" /></td>
+                <td>
+                    <span style="font-size:xx-large"><a href="<?=$projectURL?>"><?=$projectName?></a></span><br />
+                    <span style="font-size:large">Estadísticas y datos de interés</span>
+                </td>
+            </tr>
+        </table>
+        <div style="border:1px dotted; display: inline-block; padding: 15px;">
+            <p style="font-size:small;">Hasta el momento,</p>
+            <p style="font-size:large;font-weight:bold;"><?=sizeof($statsByParticipant)?> participantes</p>
+            <p style="font-size:small;">han subido</p>
+            <p style="font-size:x-large;font-weight:bold;"><?=$totalNumberOfImages?> fotografías</p>
+            <p style="font-size:small;">con las cuales han captado</p>
+            <p style="font-size:large;font-weight:bold;"><?=sizeof($statsByLocation)?> municipios</p>
+        </div>
+        <h1>Los departamentos</h1>
+
+		<table class="wikitable" style="text-align: center;">
+            <tr><th>Departamento</th><th>Fotos totales</th><th>Municipios<br />fotografiados</th></tr>
+			<?php foreach ($statsByDepartment as $nombre=>$departamento): ?>
+            <tr><td><a href="?gallery&departamento=<?=$nombre?>"><?=$nombre?></a></td><td><?=$departamento['total']?></td><td><?=$departamento['places']?></td></tr>
+			<?php endforeach; ?>
+        </table>
+        <table style="border:none;" width="90%">
+            <tr>
+                <td valign="top">
+                    <center>
+                        <h1>Los participantes</h1>
+                        <table class="wikitable" style="text-align: center;">
+                            <tr><th>Participante</th><th>Fotos totales</th><th>Municipios<br />fotografiados</th></tr>
+							<?php foreach ($statsByParticipant as $nombre=>$persona): ?>
+                            <tr><td><a href="?gallery&participante=<?=rawurlencode ($nombre)?>"><?=$nombre?></a></td><td><?=$persona['total']?></td><td><?=$persona['places']?></td></tr>
+							<?php endforeach; ?>
+                        </table>
+                    </center>
+                </td>
+                <td valign="top">
+                    <center>
+                        <h1>Los municipios</h1>
+                        <table class="wikitable" style="text-align: center;">
+                            <tr><th>Código</th><th>Municipio</th><th>Fotos totales</th><th>Participantes</th></tr>
+							<?php foreach ($statsByLocation as $id => $monumento): ?>
+                            <tr><td><a href="?gallery&municipio=<?=$id?>"><?=$id?></a></td><td><?=$monumento['name']?></td><td><?=$monumento['total']?></td><td><?=$monumento['people']?></td></tr>
+							<?php endforeach; ?>
+                        </table>
+                    </center>
+                </td>
+            </tr>
+        </table>
+    </center>
+	<?php
+
+}
+
+function getStatsByParticipant()
+{
+	global $db;
+	$stats = array();
+    $queryResult = $db->query("SELECT author,COUNT(*) total,COUNT(DISTINCT place) places FROM photos GROUP BY author ORDER BY total DESC,places DESC");
+    while ($row = $queryResult->fetchArray(SQLITE3_ASSOC))
+	{
+		$stats[$row['author']] = array('total'=>$row['total'], 'places'=>$row['places']);
+    }
+	return $stats;
+}
+
+function getStatsByZone()
+{
+	global $db;
+	$stats = array();
+    $queryResult = $db->query("select ifnull(topname,'?') topName,count(*) total,count(distinct name) places from photos left outer join places on photos.place=places.id group by topname order by total desc,places desc");
+    while ($row = $queryResult->fetchArray(SQLITE3_ASSOC))
+	{
+		$stats[$row['topName']] = array('total'=>$row['total'], 'places'=>$row['places']);
+    }
+	return $stats;
+}
+
+function getStatsByLocation()
+{
+	global $db;
+	$stats = array();
+    $queryResult = $db->query("SELECT ifnull(id,'?') id,ifnull(name,'?') name,COUNT(*) total, COUNT(DISTINCT author) people
+    FROM photos LEFT OUTER JOIN places ON photos.place=places.id GROUP BY id,name ORDER BY total DESC,people DESC");
+    while ($row = $queryResult->fetchArray(SQLITE3_ASSOC))
+	{
+	    $stats[$row['id']] = array('name'=>$row['name'], 'total'=>$row['total'], 'people'=>$row['people']);
+    }
+	return $stats;
 }
 
 function showFooter()
 {
-?>
+	?>
 	</body>
 </html>
 <?php
